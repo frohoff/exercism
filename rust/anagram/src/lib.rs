3e36ebdbd2ab739dad3aaf0d615033f0aa5654a6
Chris Frohoff <chris@frohoff.org>
2020-08-28 08:28:51 -0700

    incomplete

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,17 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    println!("check {:?} {:?}", word, possible_anagrams);
+    pub fn get_freq(word: &str) -> HashMap<char, u32> {
+        let mut freq = HashMap::new();
+        for c in word.to_lowercase().chars() {
+            *freq.entry(c).or_insert(0) += 1;
+        }
+        println!("freq {:?} {:?}", word, freq);
+        freq
+        
+    }
+    
+    let word_freq = get_freq(word);
+    possible_anagrams.iter().filter(|a| get_freq(a) == word_freq ).map(|a| *a).collect()
 }
