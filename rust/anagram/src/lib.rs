d5bead71b43d5067440cac8902548bba56bf52b4
Chris Frohoff <cfrohoff@qualcomm.com>
2020-08-28 13:19:25 -0700

    finished anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,17 +1,21 @@
 use std::collections::{HashSet, HashMap};
 
-pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    println!("check {:?} {:?}", word, possible_anagrams);
-    pub fn get_freq(word: &str) -> HashMap<char, u32> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    fn get_freq(word: &str) -> HashMap<char, u32> {
         let mut freq = HashMap::new();
-        for c in word.to_lowercase().chars() {
+        for c in word.chars() {
             *freq.entry(c).or_insert(0) += 1;
         }
-        println!("freq {:?} {:?}", word, freq);
         freq
-        
     }
-    
-    let word_freq = get_freq(word);
-    possible_anagrams.iter().filter(|a| get_freq(a) == word_freq ).map(|a| *a).collect()
+
+    let l_word = word.to_lowercase();    
+    let word_freq = get_freq(&l_word);
+    possible_anagrams.iter()
+        .filter(|a| { 
+            let l_a = a.to_lowercase(); 
+            get_freq(&l_a) == word_freq && l_word != l_a 
+        })
+        .map(|&a| a)
+        .collect()
 }
