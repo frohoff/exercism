c6535022f17bf8e3c1b8a306dfc75a576625a726
Chris Frohoff <cfrohoff@qualcomm.com>
2020-08-28 15:01:07 -0700

    alt anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,20 +1,18 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    fn get_freq(word: &str) -> HashMap<char, u32> {
-        let mut freq = HashMap::new();
-        for c in word.chars() {
-            *freq.entry(c).or_insert(0) += 1;
-        }
-        freq
+    fn sorted(s: &str) -> String {
+        let mut sorted: Vec<_> = s.chars().collect();
+        sorted.sort();
+        sorted.iter().collect::<String>()
     }
-
-    let l_word = word.to_lowercase();    
-    let word_freq = get_freq(&l_word);
+    let l_word = word.to_lowercase();
+    let s_word = sorted(&l_word);
+    
     possible_anagrams.iter()
         .filter(|a| { 
             let l_a = a.to_lowercase(); 
-            get_freq(&l_a) == word_freq && l_word != l_a 
+            sorted(&l_a) == s_word && l_word != l_a 
         })
         .map(|&a| a)
         .collect()
