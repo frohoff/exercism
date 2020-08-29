0acfd13be6a581c128b564e2759fd027b0e465e4
Chris Frohoff <cfrohoff@qualcomm.com>
2020-08-28 17:37:24 -0700

    space-age with macros

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -18,20 +18,18 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($id:ident, $e:expr) => {
+        pub struct $id;
+        impl Planet for $id { fn factor() -> f64 { $e } }
+    }
+}
 
-impl Planet for Mercury { fn factor() -> f64 { 0.2408467 } }
-impl Planet for Venus { fn factor() -> f64 { 0.61519726 } }
-impl Planet for Earth { fn factor() -> f64 { 1.0 } }
-impl Planet for Mars { fn factor() -> f64 { 1.8808158 } }
-impl Planet for Jupiter { fn factor() -> f64 { 11.862615 } }
-impl Planet for Saturn { fn factor() -> f64 { 29.447498 } }
-impl Planet for Uranus { fn factor() -> f64 { 84.016846 } }
-impl Planet for Neptune { fn factor() -> f64 { 164.79132 } }
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
